7279b0c951b7022779f2bdc5382406259ae25921
Grzegorz <grzegorz.muraczewski@gmail.com>
2018-06-02 19:54:27 +0200

    [Extension methods] Add datetime extension method

---
+++ Trainings/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Trainings
+{
+    public static class DateTimeExtensions
+    {
+        public static bool IsWeekend(this DateTime @dateTime)
+        {
+            return dateTime.DayOfWeek == DayOfWeek.Saturday || dateTime.DayOfWeek == DayOfWeek.Sunday;
+        }
+
+        public static bool IsFirstPartOfMonth(this DateTime @dateTime)
+        {
+            var numberOfDaysInCurrentMonth = DateTime.DaysInMonth(dateTime.Year, dateTime.Month);
+
+            return dateTime.Day < (numberOfDaysInCurrentMonth / 2);
+        }
+    }
+}
--- Trainings/Program.cs
+++ Trainings/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Trainings
 {
@@ -10,6 +6,19 @@ namespace Trainings
     {
         static void Main(string[] args)
         {
+            ExtensionMethodDemo();
+        }
+
+        static void ExtensionMethodDemo()
+        {
+            var today = DateTime.Today;
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Today is {today.DayOfWeek} {today.Day} of month. Is it weekend: {today.IsWeekend()}. Is it first part of month: {today.IsFirstPartOfMonth()}");
+                today = today.AddDays(8);
+            }
+            Console.ReadLine();
         }
     }
 }
