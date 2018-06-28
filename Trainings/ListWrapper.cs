11e7476c27b0ad0fc2f7841335fac4d6727f6e43
Grzegorz <grzegorz.muraczewski@gmail.com>
2018-06-28 11:39:22 +0200

    List wrapper

--- Trainings/ListWrapper.cs
+++ Trainings/ListWrapper.cs
@@ -1,6 +1,46 @@
-﻿namespace Trainings
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Trainings
 {
-    class ListWrapper
+    public class ListWrapper<T> where T : IRequiredFields, new()
     {
+        private List<T> _list;
+
+        public ListWrapper()
+        {
+            _list = new List<T>();
+        }
+
+        public ListWrapper(List<T> list)
+        {
+            _list = list;
+        }
+
+        public void Add(T element)
+        {
+            _list.Add(element);
+        }
+
+        public void Remove(T element)
+        {
+            _list.Remove(element);
+        }
+
+        public T Get(int id)
+        {
+            return _list.FirstOrDefault(l => l.Id == id);
+        }
+
+        public void AddEmpty()
+        {
+            _list.Add(new T());
+        }
+
+    }
+
+    public interface IRequiredFields
+    {
+        int Id { get; }
     }
 }
