0531b3aed94899a9a8eeefc1278714a063170ec0
Grzegorz Muraczewski <gmuraczewski@pgs-soft.com>
2019-04-02 09:35:25 +0200

    Authorize attribute

---
+++ RestApiCoreTrainings/RestApiCoreTrainings/Authorization/SupportedBrowserAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApiCoreTrainings.Filters;
+
+namespace RestApiCoreTrainings.Authorization
+{
+    public class SupportedBrowserAttribute : TypeFilterAttribute
+    {
+        public SupportedBrowserAttribute() : base(typeof(SupportedBrowserFilter))
+        {
+        }
+    }
+}
