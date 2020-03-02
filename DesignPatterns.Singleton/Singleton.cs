98cde586c3fe45f3462d5970f5e514d0dc929ccf
Grzegorz Muraczewski <gmuraczewski@pgs-soft.com>
2020-03-02 08:02:32 +0100

    [Design patterns] Signleton - two kinds

---
+++ DesignPatterns.Singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private static int _count;
+
+        public static Singleton GetInstance => _instance ?? (_instance = new Singleton());
+
+        private Singleton()
+        {
+            _count++;
+        }
+
+        public int GetCount => _count;
+    }
+}
---
+++ DesignPatterns.Singleton/SingletonWithDoubleCheck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton
+{
+    public class SingletonWithDoubleCheck
+    {
+        private static SingletonWithDoubleCheck _instance;
+        private static readonly object Lock = new object();
+        private static int _count;
+
+        public static SingletonWithDoubleCheck GetInstance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (Lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new SingletonWithDoubleCheck();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private SingletonWithDoubleCheck()
+        {
+            _count++;
+            Console.WriteLine($"Number of instances: {_count}");
+        }
+
+        public int GetCount => _count;
+    }
+}
